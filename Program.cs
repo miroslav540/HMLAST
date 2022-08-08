040b17ab50d6724bf8e5afb21e1a39f735cfdd06
Miroslav540 <mir19972905@mail.ru>
2022-08-08 11:44:59 +0300

    Code2

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿string[] array1 = new string[5] {"123", "23", "hello", "world", "res"};
+﻿string[] array1 = new string[5] {"Russia", "233", "hello", "world", ":-)"};
 string[] array2 = new string[array1.Length];
 void SecondArrayWithIF(string[] array1, string[] array2)
 {
